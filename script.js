3e54d55bff545298c6ca4d8f81c978a3f98e7751
Anastasiya <anasatsya.melenteva@gmail.com>
2018-02-08 17:03:05 -0500

    Fix second and third j.ball coordinates

--- script.js
+++ script.js
@@ -69,13 +69,7 @@ trajectories.even = [
     height: -750
 }]
 
-// Extract input values
-// trajectories.extractHeight = function() {
-//     trajectories.ballOneYtrajectory = $('input[name=ball1]').val();
-//     trajectories.ballTwoYtrajectory = $('input[name=ball2').val();
-//     trajectories.ballThreeYtrajectory = $('input[name=ball3').val();
-//     return ballOneYtrajectory, ballTwoYtrajectory, ballThreeYtrajectory;
-// };
+
 // Identify current position of each ball and middle of the screen
 // To be used to identify direction of a juggling ball (lift/ right)
 trajectories.OffsetTest = $('.ball1').offset().left;
@@ -95,10 +89,11 @@ trajectories.ballThree = document.querySelector(".ball3");
 // Identify whether a juglling ball is expected to change its vertical
 // And identify height of each throw
 trajectories.calculateCoordinates = function() {
+    // Retrieve inputted site-swap values
     ballOneYtrajectory = $('input[name=ball1]').val();
-    console.log(`here is the number ${ballOneYtrajectory}`);
     ballTwoYtrajectory = $('input[name=ball2').val();
     ballThreeYtrajectory = $('input[name=ball3').val();
+    // Calculate height and vertical 
     if (ballOneYtrajectory % 2 == 1) {
         if (trajectories.OffsetTest < trajectories.middleScreen) {
             ballOneXtrajectory = $('.container').width() - 250 + "px";
@@ -107,7 +102,8 @@ trajectories.calculateCoordinates = function() {
         else if (trajectories.OffsetTest >= middleScreen) {
             ballOneXInitiaPosition = 0;
         }
-        // Iterate through array of odd notation values and update Y trajectory with px values for the corresponding notation number
+        // Iterate through array of odd notation values
+        // and update Y trajectory with px values for the corresponding notation number
         for (let i = 0; i < trajectories.odd.length; i++) {
             if (ballOneYtrajectory == trajectories.odd[i].number) {
                 ballOneYtrajectory = trajectories.odd[i].height;
@@ -122,43 +118,166 @@ trajectories.calculateCoordinates = function() {
                 ballOneYtrajectory = trajectories.even[i].height;
             }
         }
-        console.log(`Ball One Y trajectory is ${ballOneYtrajectory}`);
+    }
+    else {
+        console.log("Ball One: Such notation does not exist");
+    }
+// Create array of objects to contain (x;y) coordinates
+    let coordinatesObject = [
+        {
+            x: ballOneXtrajectory,
+            y: ballOneYtrajectory
+        },
+        {
+            x: ballOneXtrajectory,
+            y: 0
+        },
+        {
+            x: ballOneXInitiaPosition,
+            y: ballOneYtrajectory
+        },
+        {
+            x: ballOneXInitiaPosition,
+            y: 0
+        }
+    ]
+
+// Loop through the coordinates array for ball one
+    for (let i = 0; i < coordinatesObject.length; i++) {
+        setTimeout(function timer() {
+            console.time('BallOne timer');
+            let translate3dValue = "translate(" + coordinatesObject[i].x + "," + coordinatesObject[i].y + "px)";
+            console.log(`Ball One coordinatesare ${translate3dValue}`);
+            trajectories.ballOne.style.transform = translate3dValue;
+            console.timeEnd('BallOne timer');
+        }, i * 2000);
+    }
+
+    // SECOND BALL
+    if (ballTwoYtrajectory % 2 == 1) {
+        if (trajectories.OffsetBallTwo < trajectories.middleScreen) {
+            ballTwoXInitiaPosition = 0;
+        }
+        else if (trajectories.OffsetBallTwo >= trajectories.middleScreen) {
+            ballTwoXtrajectory = -1 * ($('.container').width() - 250) + "px";
+            ballTwoXInitiaPosition = 0;
+        }
+        // Iterate through array of odd notation values
+        // and update Ytrajectory with px values for the corresponding notation number
+        for (let i = 0; i < trajectories.odd.length; i++) {
+            if (ballTwoYtrajectory == trajectories.odd[i].number) {
+                ballTwoYtrajectory = trajectories.odd[i].height;
+            }
+        }
+    }
+    else if (ballTwoYtrajectory % 2 == 0) {
+        ballTwoXtrajectory = 0;
+        ballTwoXInitiaPosition = 0;
+        for (let i = 0; i < trajectories.even.length; i++) {
+            if (ballTwoYtrajectory == trajectories.even[i].number) {
+                ballTwoYtrajectory = trajectories.even[i].height;
+            }
+        }
     }
     else {
         console.log("Such notation does not exist");
     }
 
-let coordinatesObject = [
-    {
-        x: ballOneXtrajectory,
-        y: ballOneYtrajectory
-    },
-    {
-        x: ballOneXtrajectory,
-        y: 0
-    },
-    {
-        x: ballOneXInitiaPosition,
-        y: ballOneYtrajectory
-    },
-    {
-        x: ballOneXInitiaPosition,
-        y: 0
-    }
-]
+// Create array of objects to contain (x;y) coordinates
+    const coordinatesObjectBallTwo = [
+        {
+            x: ballTwoXtrajectory,
+            y: ballTwoYtrajectory
+        },
+        {
+            x: ballTwoXtrajectory,
+            y: 0
+        },
+        {
+            x: ballTwoXInitiaPosition,
+            y: ballTwoYtrajectory
+        },
+        {
+            x: ballTwoXInitiaPosition,
+            y: 0
+        }
+    ]
+// Loop through the coordinates array for ball two
+    setTimeout(function timer() {
+        for (let i = 0; i < coordinatesObjectBallTwo.length; i++) {
+            setTimeout(function timer() {
+                console.time('BallTwo timer');
+                let translate3dValue = "translate(" + coordinatesObjectBallTwo[i].x + "," + coordinatesObjectBallTwo[i].y + "px)";
+                trajectories.ballTwo.style.transform = translate3dValue;
+                console.log(`Ball Two coordinatesare ${translate3dValue}`);
+                console.timeEnd('BallTwo timer');
+            }, i * 2000);
+        }
+    }, 500)
 
-// Loop through array for ball1
-    for (let i = 0; i < coordinatesObject.length; i++) {
-        // console.time('BallOne');
-        setTimeout(function timer() {
-            console.time('BallOne');
-            let translate3dValue = "translate(" + coordinatesObject[i].x + "," + coordinatesObject[i].y + "px)";
-            console.log(`Ball One coordinatesare ${translate3dValue}`);
-            trajectories.ballOne.style.transform = translate3dValue;
-            console.timeEnd('BallOne');
-        }, i * 2000);
-        // console.timeEnd('BallOne');
+// BALL THREE
+// Calculate height and vertical 
+    if (ballThreeYtrajectory % 2 == 1) {
+        if (trajectories.OffsetBallThree < trajectories.middleScreen) {
+            ballThreeXtrajectory = $('.container').width() - 325 + "px";
+            ballThreeXInitiaPosition = 0;
+        }
+        else if (trajectories.OffsetBallThree >= trajectories.middleScreen) {
+            ballThreeXInitiaPosition = 0;
+        }
+        // Iterate through array of odd notation values
+        // and update Ytrajectory with px values for the corresponding notation number
+        for (let i = 0; i < trajectories.odd.length; i++) {
+            if (ballThreeYtrajectory == trajectories.odd[i].number) {
+                ballThreeYtrajectory = trajectories.odd[i].height;
+            }
+        }
     }
+    else if (ballThreeYtrajectory % 2 == 0) {
+        ballThreeXtrajectory = 0;
+        ballThreeXInitiaPosition = 0;
+        for (let i = 0; i < odd.length; i++) {
+            if (ballThreeYtrajectory == trajectories.even[i].number) {
+                ballThreeYtrajectory = trajectories.even[i].height;
+            }
+        }
+    }
+    else {
+        console.log("Such notation does not exist");
+    }
+
+// Create array of objects to contain (x;y) coordinates
+    const coordinatesObjectBallThree = [
+        {
+            x: ballThreeXtrajectory,
+            y: ballThreeYtrajectory
+        },
+        {
+            x: ballThreeXtrajectory,
+            y: 0
+        },
+        {
+            x: ballThreeXInitiaPosition,
+            y: ballThreeYtrajectory
+        },
+        {
+            x: ballThreeXInitiaPosition,
+            y: 0
+        }
+    ]
+// Loop through array for ball three
+    setTimeout(function timer() {
+        for (let i = 0; i < coordinatesObjectBallThree.length; i++) {
+            setTimeout(function timer() {
+                console.time('BallThree timer');
+                let translate3dValue = "translate(" + coordinatesObjectBallThree[i].x + "," + coordinatesObjectBallThree[i].y + "px)";
+                console.log(`Ball 3 coordinates are ${translate3dValue}`);
+                trajectories.ballThree.style.transform = translate3dValue;
+                console.log(`Ball Three coordinatesare ${translate3dValue}`);
+                console.timeEnd('BallThree timer');
+            }, i * 2000);
+        }
+    }, 1000)
 }
 
 // When document is ready...
@@ -174,139 +293,5 @@ $(function(){
         // Clear input fields
         trajectories.calculateCoordinates();
         resetInput();
-        // SECOND BALL
-        // if (ballTwoYtrajectory % 2 == 1) {
-        //     if (OffsetBallTwo < middleScreen) {
-        //         // ballTwoXtrajectory = 0;
-        //         ballTwoXInitiaPosition = 0;
-        //     }
-        //     else if (OffsetBallTwo >= middleScreen) {
-        //         ballTwoXtrajectory = -1 * ($('.container').width() - 250) + "px";
-        //         ballTwoXInitiaPosition = 0;
-        //     }
-        //     // Iterate through array of odd notation values and update Ytrajectory with px values for the corresponding notation number
-        //     for (let i = 0; i < odd.length; i++) {
-        //         if (ballTwoYtrajectory == odd[i].number) {
-        //             ballTwoYtrajectory = odd[i].height;
-        //         }
-        //     }
-        //     // console.log(`Ball Two trajectory ${ballTwoYtrajectory}`);
-        // }
-        // else if (ballTwoYtrajectory % 2 == 0) {
-        //     ballTwoXtrajectory = 0;
-        //     ballTwoXInitiaPosition = 0;
-        //     for (let i = 0; i < odd.length; i++) {
-        //         if (ballTwoYtrajectory == even[i].number) {
-        //             ballTwoYtrajectory = even[i].height;
-        //         }
-        //     }
-        //     console.log(`Ball Two trajectory ${ballTwoYtrajectory}`);
-        // }
-        // else {
-        //     console.log("Such notation does not exist");
-        // }
-
-        // // Create array with coordinates
-        // const coordinatesObjectBallTwo = [
-        //     {
-        //         x: ballTwoXtrajectory,
-        //         y: ballTwoYtrajectory
-        //     },
-        //     {
-        //         x: ballTwoXtrajectory,
-        //         y: 0
-        //     },
-        //     {
-        //         x: OffsetBallTwo,
-        //         y: 0
-        //     },
-        //     {
-        //         x: ballTwoXInitiaPosition,
-        //         y: ballTwoYtrajectory
-        //     },
-        //     {
-        //         x: ballTwoXInitiaPosition,
-        //         y: 0
-        //     }
-        // ]
-        // // Loop through array for ball1
-        // setTimeout(function timer() {
-        //     for (let i = 0; i < coordinatesObjectBallTwo.length; i++) {
-        //         setTimeout(function timer() {
-        //             let translate3dValue = "translate(" + coordinatesObjectBallTwo[i].x + "," + coordinatesObjectBallTwo[i].y + "px)";
-        //             // console.log(translate3dValue);
-        //             ballTwo.style.transform = translate3dValue;
-        //         }, i*2000);
-        //     }
-        // }, 500)
-
-        // // END OF THE SECOND BALL
-
-        // // SECOND BALL
-        // if (ballThreeYtrajectory % 2 == 1) {
-        //     if (OffsetBallThree < middleScreen) {
-        //         ballThreeXtrajectory = $('.container').width() - 325 + "px";
-        //         ballThreeXInitiaPosition = 0;
-        //     }
-        //     else if (OffsetBallThree >= middleScreen) {
-        //         // ballThreeXtrajectory = 0;
-        //         ballThreeXInitiaPosition = 0;
-        //     }
-        //     // Iterate through array of odd notation values and update Ytrajectory with px values for the corresponding notation number
-        //     for (let i = 0; i < odd.length; i++) {
-        //         if (ballThreeYtrajectory == odd[i].number) {
-        //             ballThreeYtrajectory = odd[i].height;
-        //         }
-        //     }
-        //     // console.log(`Ball 3 Y trajectory is ${ballThreeYtrajectory}`);
-        // }
-        // else if (ballThreeYtrajectory % 2 == 0) {
-        //     ballThreeXtrajectory = 0;
-        //     ballThreeXInitiaPosition = 0;
-        //     for (let i = 0; i < odd.length; i++) {
-        //         if (ballThreeYtrajectory == even[i].number) {
-        //             ballThreeYtrajectory = even[i].height;
-        //         }
-        //     }
-        //     console.log(`Ball 3 Y trajectory is ${ballThreeYtrajectory}`);
-        // }
-        // else {
-        //     console.log("Such notation does not exist");
-        // }
-
-        // // Create array with coordinates
-        // const coordinatesObjectBallThree = [
-        //     {
-        //         x: ballThreeXtrajectory,
-        //         y: ballThreeYtrajectory
-        //     },
-        //     {
-        //         x: ballThreeXtrajectory,
-        //         y: 0
-        //     },
-        //     {
-        //         x: OffsetBallThree,
-        //         y: 0
-        //     },
-        //     {
-        //         x: ballThreeXInitiaPosition,
-        //         y: ballThreeYtrajectory
-        //     },
-        //     {
-        //         x: ballThreeXInitiaPosition,
-        //         y: 0
-        //     }
-        // ]
-        // // Loop through array for ball1
-        // setTimeout(function timer() {
-        //     for (let i = 0; i < coordinatesObjectBallThree.length; i++) {
-        //         setTimeout(function timer() {
-        //             let translate3dValue = "translate(" + coordinatesObjectBallThree[i].x + "," + coordinatesObjectBallThree[i].y + "px)";
-        //             console.log(`Ball 3 coordinates are ${translate3dValue}`);
-        //             ballThree.style.transform = translate3dValue;
-        //         }, i * 2000);
-        //     }
-        // }, 1000)
-
     });
 });
